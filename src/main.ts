e84b85d1071cfe9fd652ed31f1a5f2d7ca25db36
TanNH863 <nguyenhuutan080603@gmail.com>
2025-11-25 22:21:51 +0700

    modify reverse proxy

--- src/main.ts
+++ src/main.ts
@@ -4,25 +4,6 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const allowedOrigins = (
-    process.env.CORS_ORIGINS || 'http://172.23.58.104:3000'
-  )
-    .split(',')
-    .map((s) => s.trim());
-  app.enableCors({
-    origin: (origin, callback) => {
-      // when origin is undefined (e.g., server-to-server, curl), allow it
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error('Not allowed by CORS'), false);
-      }
-    },
-    credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type,Authorization',
-  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
